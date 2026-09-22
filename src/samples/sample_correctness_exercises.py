746123f478946f8aa7f046e0f1f2e883b6ac876d
Riccardo Amadio <riccardoamadio@MacBook-Pro-di-Riccardo.local>
2026-09-22 10:47:21 +0200

    wip

--- src/samples/sample_correctness_exercises.py
+++ src/samples/sample_correctness_exercises.py
@@ -6,10 +6,10 @@ from src.factories.python_correctness import PythonCorrectnessFactory
 CONFIG_DIR = Path(__file__).parent / "configs"
 
 MODELS = [
-    "bartowski/DeepSeek-Coder-V2-Lite-Instruct-GGUF:IQ2_XS", #0.65 accuracy , failed 56, success 104
-    "ggml-org/gemma-3-4b-it-GGUF", #0.80 accuracy, failed 32, success 128
-    "Qwen/Qwen2.5-Coder-7B-Instruct-GGUF", #0.76 accuracy, failed 38, success 122
-    "Qwen/Qwen2.5-Coder-14B-Instruct-GGUF", # 0.89 accuracy, failed 18, success 142
+    "bartowski/DeepSeek-Coder-V2-Lite-Instruct-GGUF:IQ2_XS",  # 0.65 accuracy , failed 56, success 104
+    "ggml-org/gemma-3-4b-it-GGUF",  # 0.80 accuracy, failed 32, success 128
+    "Qwen/Qwen2.5-Coder-7B-Instruct-GGUF",  # 0.76 accuracy, failed 38, success 122
+    "Qwen/Qwen2.5-Coder-14B-Instruct-GGUF",  # 0.89 accuracy, failed 18, success 142
 ]
 
 
